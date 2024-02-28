19b70e3c4feeb3ccc7906d204e2ebf07e11bc4b1
jeevaramanan <161497475+jeevaramanan@users.noreply.github.com>
2024-02-28 15:05:03 +0530

    Update config.js

--- src/js/config.js
+++ src/js/config.js
@@ -1,3 +1,4 @@
+import { Fraction } from 'fractional';
 export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes/';
 export const TIME_OUT = 5;
 export const SRH_URL = `https://forkify-api.herokuapp.com/api/v2/recipes?search=`;
